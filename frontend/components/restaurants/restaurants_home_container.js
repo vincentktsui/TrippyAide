136eeec4f85dd4b560deb0a6104a9efe1a6219c8
vincentktsui <vincentktsui@gmail.com>
2020-05-08 21:15:15 -0700

    WIP

---
+++ frontend/components/restaurants/restaurants_home_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+// import { fetchHotels } from '../../actions/hotel_actions';
+// import { updateFilter, updateHomeFilter } from '../../actions/filter_actions';
+// import HotelsHome from './hotels_home';
+
+
+const mapStateToProps = ({ entities, ui }) => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+});
+
+// export default connect(mapStateToProps, mapDispatchToProps)();
